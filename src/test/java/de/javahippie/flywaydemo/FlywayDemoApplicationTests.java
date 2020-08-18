e6a74291d81f2f60343bf8b5609a4f2c3c3580b8
Tim Zöller <tim.zoeller@ilume.de>
2020-08-18 18:57:39 +0200

    Initial Commit

---
+++ src/test/java/de/javahippie/flywaydemo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.javahippie.flywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
