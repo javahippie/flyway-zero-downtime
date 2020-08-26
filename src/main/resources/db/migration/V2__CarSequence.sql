cc5f64f902014b4683d3de8eb6dcb3111707cdc6
Tim Zöller <tim.zoeller@ilume.de>
2020-08-26 20:27:39 +0200

    Fixed App Some More

---
+++ src/main/resources/db/migration/V2__CarSequence.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  zoeller
+ * Created: 18.08.2020
+ */
+
+CREATE SEQUENCE seq_car START WITH 1 INCREMENT BY 1;
